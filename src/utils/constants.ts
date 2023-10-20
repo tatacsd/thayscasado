91b1065d0ef9f46cb58398996e3dac4cc96caf8d
Thays Casado <thayscasado@gmail.com>
2023-10-19 23:28:47 -0700

    🚀

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,8 +3,6 @@ export const SECTIONS = {
     CONTACT: "CONTACT",
     PROJECTS: "PROJECTS",
   };
-
-
   export const textArray = ["Developer.", "Freelancer.", "Designer.", "Photographer."];
   export const pdfUrl =
   "https://drive.google.com/file/d/1b1XGWlwcwAF5EIYttntD4vEIpRx2QvbB/view?usp=drive_link";
